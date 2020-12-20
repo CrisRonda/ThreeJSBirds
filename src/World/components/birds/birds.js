ca4d83924f85f3e8cd58a29f15a4af06fb655ea3
CrisRonda <cristian.j005@gmail.com>
2020-12-19 21:42:49 -0500

    fix import models

--- src/World/components/birds/birds.js
+++ src/World/components/birds/birds.js
@@ -1,17 +1,17 @@
-import { GLTFLoader } from 'https://unpkg.com/three@0.117.0/examples/jsm/loaders/GLTFLoader.js';
+import { GLTFLoader } from "https://unpkg.com/three@0.117.0/examples/jsm/loaders/GLTFLoader.js";
 
-import { setupModel } from './setupModel.js';
+import { setupModel } from "./setupModel.js";
 
 async function loadBirds() {
   const loader = new GLTFLoader();
 
   const [parrotData, flamingoData, storkData] = await Promise.all([
-    loader.loadAsync('/assets/models/Parrot.glb'),
-    loader.loadAsync('/assets/models/Flamingo.glb'),
-    loader.loadAsync('/assets/models/Stork.glb'),
+    loader.loadAsync("../../../../assets/models/Parrot.glb"),
+    loader.loadAsync("../../../../assets/models/Flamingo.glb"),
+    loader.loadAsync("../../../../assets/models/Stork.glb"),
   ]);
 
-  console.log('Squaaawk!', parrotData);
+  console.log("Squaaawk!", parrotData);
 
   const parrot = setupModel(parrotData);
   parrot.position.set(0, 0, 2.5);
